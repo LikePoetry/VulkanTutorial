ebdecf5174d48a1f7e850e579b4fc2e76424588b
shenjingye <249857765@qq.com>
2022-10-26 23:39:43 +0800

    add mouseclick event

---
+++ PointTest/shaders/ShaderBuild.bat
@@ -0,0 +1,4 @@
+E:/VulkanSDK/1.3.224.1/Bin/glslc.exe shader.vert -o vert.spv
+E:/VulkanSDK/1.3.224.1/Bin/glslc.exe shader.frag -o frag.spv
+
+pause
--- premake5.lua
+++ premake5.lua
@@ -152,3 +152,66 @@ project "SandboxEdit"
 		defines ""
 		runtime "Release"
 		optimize "on"
+
+
+project "PointTest"
+
+		location "PointTest"
+		kind "ConsoleApp"
+		language "C++"
+		cppdialect "C++17"
+		staticruntime "on"
+		characterset ("MBCS")
+	
+		targetdir("bin/" ..outputdir.. "/%{prj.name}")
+		objdir("bin-int/" ..outputdir.. "/%{prj.name}")
+		
+	
+		files
+		{
+			"%{prj.name}/**.h",
+			"%{prj.name}/**.cpp",
+		}
+	
+		includedirs
+		{
+			"%{prj.name}",
+			"vendor/spdlog/include",
+			"%{IncludeDir.GLFW}",
+			"%VULKAN_SDK%/include",
+			"%{IncludeDir.glm}",
+			"%{IncludeDir.imgui}",
+			"%{IncludeDir.stb}",
+	
+		}
+	
+		links
+		{
+			"GLFW",
+			"ImGui",
+			"vulkan-1.lib"
+		}
+	
+		libdirs 
+		{ 
+			"%VULKAN_SDK%/lib" 
+		}
+	
+		filter "system:windows"
+			systemversion "latest"
+	
+			defines
+			{
+				GLFW_INCLUDE_NONE
+			}
+	
+	
+		filter "configurations:Debug"
+			defines ""
+			runtime "Debug"
+			symbols "on"
+	
+		filter "configurations:Release"
+			defines ""
+			runtime "Release"
+			optimize "on"
